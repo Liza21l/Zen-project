e8e70b00cbaf1c136ee613c4281c68e222dda7fa
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2024-04-17 09:36:42 +0300

    commit Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import PlaceAnOrder from './Components/PlaceAnOrder/PlaceAnOrder';
-// import Header from './Components/Header/Header';
+
 const App = () => {
     return (
         <>
-        {/* <Header/> */}
+        
         <PlaceAnOrder/>
         </>
     )
